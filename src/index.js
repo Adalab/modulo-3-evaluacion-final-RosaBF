002b23da0105050d35ae58ba118d83172ec114cb
RosaBF <rosaberned30@gmail.com>
2021-06-15 17:52:05 +0200

    add first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import './stylesheets/index.css';
+import App from './components/App';
+
+//desde index.js-->importa y pinta--> App
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById('root')
+);
